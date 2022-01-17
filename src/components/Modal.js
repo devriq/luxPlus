7d01cae8bc947164ff55118434ba22bd660abad7
devriq <devriq.cordovil@gmail.com>
2022-01-17 13:49:25 -0300

    modal updated

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,9 +3,13 @@ import './Modal.css'
 
 
 export default ({ status, nome, poster }) => {
+    function closeModal() {
+        let modal = document.querySelector('.modal')
+        modal.style.display = 'none';
+    }
     return(
         <div className='modal'>
-            <img src="https://www.themoviedb.org/t/p/w500/qqqE8jR2bzZldtW5lJwKhHMxkx0.jpg" id='poster' alt='img'/>
+            <img onClick={closeModal} src="https://www.themoviedb.org/t/p/w500/qqqE8jR2bzZldtW5lJwKhHMxkx0.jpg" id='poster' alt='img'/>
             <div id='description'>
                 <div id='title'>Pânico 4</div>
                 <div id='overview'>Faz muitos anos que o assassino Ghostface deixou um rastro mortal pela cidade de Woodsboro. A fim de superar o trauma desses terríveis acontecimentos, Sidney Prescott escreveu um livro de auto-ajuda. Ela retorna à Woodsboro para lançar seu livro e reencontra os velhos amigos Gale Weathers e o Xerife Dewey. No entanto, a chegada de Sidney também provoca o retorno de Ghostface, colocando Sidney e todos que ama em perigo.</div>
--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './MovieShow.css'
-
+import Modal from './Modal';
 
 export default ({ status, nome, poster }) => {
+  function openModal() {
+    <Modal/>
+    let modal = document.querySelector('.modal')
+    modal.style.display = 'flex';
+  }
   return (
     <div className="container">
       <div className="movieShow">
-        <img onclick="openModal()" className={status} src={poster} alt="poster" />
+        <img onClick={openModal} className={status} src={poster} alt="poster" />
         <h4 className={status}>{nome}</h4>
       </div>
     </div>
