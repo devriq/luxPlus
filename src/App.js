e9f43e5f2f7bbad0798f5e58653b844cb1fcc5ec
devriq <devriq.cordovil@gmail.com>
2022-01-17 12:43:30 -0300

    modal added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import MovieShow from './components/MovieShow'
+import Modal from './components/Modal';
 import './App.css'
 
 const data = require('./data.json');
@@ -17,8 +18,13 @@ export default () => {
     loadAll()
   }, [])
 
+  function openModal() {
+    let modal = document.querySelector('.modal')
+    modal.style.display = 'flex';
+  }
   return (
     <div className="page">
+      <Modal/>
       <nav>
         <img src="https://document-export.canva.com/B_Jl0/DAE1ejB_Jl0/3/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20220114%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220114T164948Z&X-Amz-Expires=39543&X-Amz-Signature=1bc1c05eacb6ef6663e12c03396a1b5530fed010e0a43c538e6fa69e4844e416&X-Amz-SignedHeaders=host&response-expires=Sat%2C%2015%20Jan%202022%2003%3A48%3A51%20GMT" alt="logo"/>
         <ul>
@@ -26,7 +32,7 @@ export default () => {
           <li>1995 - 2010</li>        
           <li>2011 - 2021</li>
         </ul>
-        <button>Search</button>        
+        <button>Add</button>        
       </nav>
       <section className="list">
         {movieList.map((item, key) => (
--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -6,7 +6,7 @@ export default ({ status, nome, poster }) => {
   return (
     <div className="container">
       <div className="movieShow">
-        <img className={status} src={poster} alt="poster" />
+        <img onclick="openModal()" className={status} src={poster} alt="poster" />
         <h4 className={status}>{nome}</h4>
       </div>
     </div>
