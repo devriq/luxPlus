86b660d554a1bbf28a66a9a968f7b64b98c64f68
devriq <devriq.cordovil@gmail.com>
2022-01-20 23:23:32 -0300

    site API linked to List

--- src/App.js
+++ src/App.js
@@ -4,55 +4,56 @@ import Modal from './components/Modal';
 import './App.css'
 
 const data = require('./data.json');
-
+const API = 'https://api.themoviedb.org/4/list/8175818?page=1&api_key=a79b231633cd9524b54133ecc5c8f1a5&language=pt-BR&sort_by=release_date.asc'
 
 export default () => {
   const [movieList, setMovieList] = useState([])
   const [k,setK] = useState('0');
-  useEffect(() => {
-    const loadAll = () => {
-      let list = data
-      setMovieList(list)
-    }
 
-    loadAll()
+  useEffect(() => {
+    fetch(API)
+      .then((res)=>res.json())
+      .then((data) => {
+        console.log(data['results']);
+        setMovieList(data['results']);
+      })
   }, [])
 
-  function showList1(e) {
+  function handleClick1(e) {
     e.preventDefault();
-    setK('1');
-    
-  }
-  function showList2(e) {
+    setK('1');}
+  function handleClick2(e) {
     e.preventDefault();
-    setK('2');
-    
-  }
-  function showList3(e) {
+    setK('2');}
+  function handleClick3(e) {
     e.preventDefault();
-    setK('3');
+    setK('3');}
+  function handleClick4(e) {
+    e.preventDefault();
+    setK('4');}
     
-  }
-
 
   return (
     <div className="page">
-      <Modal/>
+      
       <nav>
         <a href='https://devriq.github.io/luxPlus'><img src="https://document-export.canva.com/B_Jl0/DAE1ejB_Jl0/3/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20220114%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220114T164948Z&X-Amz-Expires=39543&X-Amz-Signature=1bc1c05eacb6ef6663e12c03396a1b5530fed010e0a43c538e6fa69e4844e416&X-Amz-SignedHeaders=host&response-expires=Sat%2C%2015%20Jan%202022%2003%3A48%3A51%20GMT" alt="logo"/></a>
         <ul>
-          <li onClick={showList1}>1960 - 1994</li>        
-          <li onClick={showList2}>1995 - 2010</li>        
-          <li onClick={showList3}>2011 - 2021</li>
+          <li onClick={handleClick1}>1960-1994</li>
+          <li onClick={handleClick2}>1995-2010</li>
+          <li onClick={handleClick3}>2011-2020</li>
+          <li onClick={handleClick4}>2021+</li>
         </ul>
-        <button>Add</button>        
+        <button>Search</button>        
       </nav>
-      <section className="list">
-        {movieList.map((item, key) => (item.listNumber === k && <MovieShow status={item.status} key={key} nome={item.title} poster={item.poster} overview={item.overview} />))}
-        
+      <section className="list"> 
+        {movieList.map((movie) => (k=='1'&& parseInt(movie.release_date.substr(0,4))<1995) && <MovieShow key={movie.id} {...movie} />)}
+        {movieList.map((movie) => (k=='2'&& parseInt(movie.release_date.substr(0,4))<2010 && parseInt(movie.release_date.substr(0,4))>=1995) && <MovieShow key={movie.id} {...movie} />)}
+        {movieList.map((movie) => (k=='3'&& parseInt(movie.release_date.substr(0,4))<2020 && parseInt(movie.release_date.substr(0,4))>=2010) && <MovieShow key={movie.id} {...movie} />)}
+        {movieList.map((movie) => (k=='4'&& parseInt(movie.release_date.substr(0,4))>=2020) && <MovieShow key={movie.id} {...movie} />)}
       </section>
       <footer>
-        {k!=='0' && <h6>Copyright © Devriq. All rights reserved.</h6>}
+        {k!=='0' && <h6>Powered by ©. All rights reserved.</h6>}
       </footer>
     </div>
   )
--- src/components/MovieShow/index.js
+++ src/components/MovieShow/index.js
@@ -1,17 +1,21 @@
 import './MovieShow.css'
 
-export default function ({status, nome, poster, overview}) {
+export default function ({status, original_title, poster_path, overview}) {
   function openModal() {
     let modal = document.querySelector('.modal');
     modal.style.display = 'flex';
-    document.getElementById("poster").src = poster;
-    document.getElementById("title").innerText = nome;
+    document.getElementById("poster").src = "https://image.tmdb.org/t/p/w500"+poster_path;
+    document.getElementById("title").innerText = original_title;
     document.getElementById("overview").innerText = overview;
   }
   return (
     <div className="movieShow">
-        <img onClick={openModal} className={status} src={poster} alt="poster" />
-        <h4 className={status}>{nome}</h4>
+        <img onClick={openModal} className={status} src={"https://image.tmdb.org/t/p/w500"+poster_path} alt="poster" />
+        <h3>{original_title}</h3>
+        <div onClick={openModal} className='overviewInfo'>
+          <h3>Sinopse</h3>
+          <p>{overview}</p>
+        </div>
     </div>
   )
 }
