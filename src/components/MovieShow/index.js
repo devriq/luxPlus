6cae819a63f6268ae981b7eeb3c9330639dd0e8b
devriq <devriq.cordovil@gmail.com>
2022-01-29 05:03:03 -0300

    btn removed

--- src/components/MovieShow/index.js
+++ src/components/MovieShow/index.js
@@ -2,23 +2,14 @@ import React from 'react';
 import './MovieShow.css'
 
 export default ({status, original_title, poster_path, overview, release_date, id}) => {
-  function openModal() {
-    let modal = document.querySelector('.modal');
-    modal.style.display = 'flex';
-    document.getElementById("poster").src = "https://image.tmdb.org/t/p/w500"+poster_path;
-    document.getElementById("title").innerText = original_title;
-    document.getElementById("overview").innerText = overview;
-  }
-
   var a = `movie:${id}`
   //console.log(status['comments'][a])
   
   return (
     <div className="movieShow">
-        <img onClick={openModal} className={status['comments'][a]} src={"https://image.tmdb.org/t/p/w500"+poster_path} alt="poster" />
-        <h3>{original_title} ({release_date.substr(0,4)})</h3>
-        <div onClick={openModal} className='overviewInfo'>
-          <h3>Sinopse</h3>
+        <img className={status['comments'][a]} src={"https://image.tmdb.org/t/p/w500"+poster_path} alt="poster" />
+        <div className='overviewInfo'>
+          <h2>{original_title} ({release_date.substr(0,4)})</h2>
           <p>{overview}</p>
         </div>
     </div>
